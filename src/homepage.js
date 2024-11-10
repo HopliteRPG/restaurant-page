27e06715f88a77600df7bc4e493d595db01d914c
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-11-09 22:48:09 -0500

    Made a function that creates a div

--- src/homepage.js
+++ src/homepage.js
@@ -5,11 +5,11 @@ import pizzaPhoto from "./photos/pizza.jpg";
 
 
 function createHomepage (){
-    let contentHTML = document.querySelector("#content");
+    const contentHTML = document.querySelector("#content");
 
-    let divCreate = document.createElement("div");
+    const divCreate = document.createElement("div");
 
-    let h1Create = document.createElement("h1")
+    const h1Create = document.createElement("h1")
 
 
     const imageVar = document.createElement("img");
@@ -17,38 +17,16 @@ function createHomepage (){
     imageVar.setAttribute("class","pizza-photo")
 
 
-    let pCreate = document.createElement("p");
+    const pCreate = document.createElement("p");
 
     
 
-    function createDivSection(html,div,p,ptext,h1,h1text,img){
-        let cloneDivCreate = div.cloneNode(true);
-        let cloneH1Create = h1.cloneNode(true);
-        let clonePCreate = p.cloneNode(true);
-
-        if(img == undefined){
-            html.appendChild(cloneDivCreate);
-            cloneDivCreate.appendChild(cloneH1Create);
-            cloneH1Create.innerText = h1text;
-            cloneDivCreate.appendChild(clonePCreate);
-            clonePCreate.innerText = ptext;
-        }
-        else{
-            html.appendChild(cloneDivCreate);
-            cloneDivCreate.appendChild(cloneH1Create);
-            cloneH1Create.innerText = h1text;
-            cloneDivCreate.appendChild(img);    
-            cloneDivCreate.appendChild(clonePCreate);
-            clonePCreate.innerText = ptext;
-        }
-        
-       
+    function createDivSection(content,div){
+        const cloneDivCreate = div.cloneNode(true);
+        content.appendChild(cloneDivCreate);
     }
 
-    createDivSection(contentHTML,divCreate,pCreate,"Sams Pizza is the best Pizza in all of Maine",h1Create,"Sams Pizza",imageVar);
-    createDivSection(contentHTML,divCreate,pCreate,"Monday-Sunday: 6am-7pm",h1Create,"Hours Open",undefined);
-    createDivSection(contentHTML,divCreate,pCreate,"123 Forest Drive, Forestville, Maine",h1Create,"Location",undefined);
-
+    const descriptionDiv = createDivSection(contentHTML,divCreate);
 
 
 
