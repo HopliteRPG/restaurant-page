048b6e46c73ef969189a1953dab2d0c0ee3055da
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-11-12 20:31:07 -0500

    createP and createH1 working

--- src/homepage.js
+++ src/homepage.js
@@ -23,10 +23,36 @@ function createHomepage (){
 
     function createDivSection(content,div){
         const cloneDivCreate = div.cloneNode(true);
+        clearDOM(content);
         content.appendChild(cloneDivCreate);
+        return{content,div}
     }
 
+    function createP(content,div,p,text){
+        const clonePCreate = p.cloneNode(true);
+        clearDOM(content);
+        clonePCreate.innerHTML = text
+        div.appendChild(clonePCreate);
+        content.appendChild(div);
+    }
+
+    function createH1(content,div,h1,text){
+        const cloneH1Create = h1.cloneNode(true);
+        clearDOM(content);
+        cloneH1Create.innerText = text;
+        div.appendChild(cloneH1Create);
+        content.appendChild(div);
+
+    }
+    
+    function clearDOM(content){
+        content.innerHTML = "";
+    }
+
+    
     const descriptionDiv = createDivSection(contentHTML,divCreate);
+    const descriptionP = createP(content,descriptionDiv.div,pCreate,"Were the best pizza joint in all of Maine")
+    const descriptionP2 = createP(content,descriptionDiv.div,pCreate,"And that shit is based");
 
 
 
