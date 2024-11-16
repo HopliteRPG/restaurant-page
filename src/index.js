887831f012b066fbfa9fdec723220c7050dfcc25
Bill <146594930+HopliteRPG@users.noreply.github.com>
2024-11-15 19:16:55 -0500

    when the home button is clicked the content is cleared and createHomepage is called

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import "./styles.css";
 import {createHomepage} from "./homepage";
-import { createMenu } from "./menu";
+import {createMenu} from "./menu";
 
 const contentHTML = document.querySelector("#content");
+
+const homeBtn = document.querySelector(".homeBtn");
 const menuBtn = document.querySelector(".menuBtn");
+const aboutBtn = document.querySelector(".aboutBtn");
+
 
 function clearContent(content){
     content.innerHTML = "";
@@ -11,6 +15,11 @@ function clearContent(content){
 
 
 createHomepage();
+
+homeBtn.addEventListener("click", ()=>{
+    clearContent(contentHTML);
+    createHomepage()
+})
 
 menuBtn.addEventListener("click", ()=>{
     clearContent(contentHTML);
